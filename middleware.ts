0bd0ba9ab1736970988051b59d8771ca72630d6d
fauzan <fauzaan.irsyaadi@gmail.com>
2025-10-25 10:04:32 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/middleware'
-import { createClient } from './lib/supabase/server';
+import { createClient } from '@/lib/supabase/server';
 
 export async function middleware(request: NextRequest) {
   const supabase = createClient();
