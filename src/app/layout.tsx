2778666312647d49178a7bd5e5dfe3e18627879b
fauzaan irsyaadi <fauzaan.irsyaadi@gmail.com>
2025-10-27 09:15:28 +0700

    update icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet" />
+        {/* Favicon (text "C") */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body className="font-body antialiased bg-background text-foreground">
         <div className="relative flex min-h-screen flex-col">
